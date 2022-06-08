f587fe9de329eedb3aa684b92e6a5a63f8c67b3f
andersonlebon <buyananderson@gmail.com>
2022-06-08 10:38:52 +0200

    Add the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "quiz_assessments#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :quiz_assessments do
     resources :questions do
@@ -7,5 +8,4 @@ Rails.application.routes.draw do
     end
   end
   # Defines the root path route ("/")
-  root "home#index"
 end
